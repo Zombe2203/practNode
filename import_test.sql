50069cb234add7f8c695285b494f32d031c464c5
Zombe2203 <55375050+Zombe2203@users.noreply.github.com>
2024-06-09 22:14:52 +0300

    Update import_test.sql

--- import_test.sql
+++ import_test.sql
@@ -1,50 +1,63 @@
 -- Creation of a test base...
 
-CREATE DATABASE test;
+CREATE DATABASE bank;
 
-CREATE TABLE files (
-  id_file int(11) NOT NULL,
-  id_my int(11) NOT NULL,
-  description text NOT NULL,
-  name_origin text NOT NULL,
-  path text NOT NULL,
-  date_upload text NOT NULL
+CREATE TABLE individuals (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCAHR(30) NOT NULL,
+  middle_name VARCHAR(30),
+  passport VARCHAR(10) NOT NULL,
+  taxpayer_number VARCAHR(12) NOT NULL,
+  insurance_number VARCAHR(11) NOT NULL,
+  driver_licence VARCAHR(10),
+  extra_documents VARCHAR(255),
+  notes VARCHAR(255),
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=cp1251;
 
-INSERT INTO files (id_file, id_my, description, name_origin, path, date_upload) VALUES
-(16, 17, 'Закачка из менеджера', 'test file 1.pdf', 'files/test_file1.pdf', '31-03-2023  20:07:59');
+INSERT INTO individuals (first_name, last_name, middle_name, passport, taxpayer_number, insurance_number, driver_licence, extra_documents, notes) VALUES
+('Ivan', 'Ivanov', 'Ivanovich', '1234567890', '123456789012', '12345678901', null, null, null),
+('Petr', 'Petrov', 'Petrovich', '2345678901', '234567890123', '23456789012', null, null, null),
+('Oleg', 'Olegov', 'Olegovich', '3456789012', '345678901234', '34567890123', null, null, null),
+('Roman', 'Romanov', 'Romanovich', '4567890123', '456789012345', '45678901234', null, null, null),
+('Stepan', 'Stepanov', 'Stepanovich', '5678901234', '567890123456', '56789012345', null, null, null);
 
-CREATE TABLE myarttable (
-  id int(11) NOT NULL,
-  text text NOT NULL,
-  description text NOT NULL,
-  keywords text NOT NULL
+CREATE TABLE borrowers (
+  borrower_id INT NOT NULL AUTO_INCREMENT,
+  taxpayer_number VARCHAR(12) NOT NULL,
+  is_legal_entity BIT NOT NULL,
+  address VARCHAR(255) NOT NULL,
+  amount INT NOT NULL,
+  conditions VARCHAR(255) NOT NULL,
+  legal_notes VARCHAR(255),
+  contracts_list VARCHAR(255),
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=cp1251;
 
-INSERT INTO myarttable (id, text, description, keywords) VALUES
-(17, 'Baranov', 'Engeneer', 'Ivanov'),
-(20, 'Fedorov', 'Cpp, Delphi, PHP, JS', '3t'),
-(92, 'Daniel', 'Artist', 'Theater Saturday'),
-(93, 'Andrew', 'Poet', 'First Electrotechnical University'),
-(94, 'Nikita', 'Student', 'Technological Institute'),
-(95, 'Ilya', 'Salesman', 'Hypermarket'),
-(96, 'Matvey', 'Programmer', 'Metropolitan College'),
-(97, 'Fedor', 'Loader', 'St. Petersburg State University'),
-(98, 'Ivan', 'Student', 'LETI'),
-(99, 'Alexey', 'Engineer', 'ITMO');
+CREATE TABLE loans (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  individual_id INT NOT NULL,
+  amount INT NOT NULL,
+  interest DEC(5,2) NOT NULL,
+  term TIMESTAMP NOT NULL,
+  conditions VARCHAR(255) NOT NULL,
+  notes VARCHAR(255) NOT NULL,
+  borrower_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (individual_id) REFERENCES individuals(id),
+  FOREIGN KEY (borrower_id) REFERENCES borrowers(id)
+) ENGINE=InnoDB DEFAULT CHARSET=cp1251;
 
-ALTER TABLE files
-  ADD PRIMARY KEY (id_file),
-  ADD KEY id_my (id_my);
-
-ALTER TABLE myarttable
-  ADD PRIMARY KEY (id);
-
-ALTER TABLE files
-  MODIFY id_file int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
-
-ALTER TABLE myarttable
-  MODIFY id int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=100;
-
-ALTER TABLE files
-  ADD CONSTRAINT files_ibfk_1 FOREIGN KEY (id_my) REFERENCES myarttable (id);
+CREATE TABLE companies_loans (
+  id INT NOT NULL AUTO_INCREMENT,
+  company_id INT NOT NULL,
+  individual_id INT NOT NULL,
+  amount INT NOT NULL,
+  term TIMESTAMP NOT NULL,
+  interest DEC(5,2) NOT NULL,
+  conditions VARCHAR(255) NOT NULL,
+  notes VARCHAR(255),
+  PRIMARY KEY (id),
+  FOREIGN KEY (individual_id) REFERENCES individuals(id)
+) ENGINE=InnoDB DEFAULT CHARSET=cp1251;
